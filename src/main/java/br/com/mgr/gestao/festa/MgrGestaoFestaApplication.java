6c94b384efcaad42f5e37eff9dd27096c2a9be83
maurigre@gmail.com <7UUjepGDa4yquMN__!@#$4321>
2021-03-14 15:45:22 -0300

    feat: Estrutura inicial do projeto

---
+++ src/main/java/br/com/mgr/gestao/festa/MgrGestaoFestaApplication.java
@@ -0,0 +1,13 @@
+package br.com.mgr.gestao.festa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MgrGestaoFestaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MgrGestaoFestaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/mgr/gestao/festa/MgrGestaoFestaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mgr.gestao.festa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MgrGestaoFestaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
