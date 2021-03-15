ec08d692841362932042f42305f0fa10bb2ae6c8
maurigre@gmail.com <7UUjepGDa4yquMN__!@#$4321>
2021-03-14 22:46:19 -0300

    feat: adicionando autenticacao no projeto

--- src/main/java/br/com/mgr/gestao/festa/controllers/ConvidadosController.java
+++ src/main/java/br/com/mgr/gestao/festa/controllers/ConvidadosController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import br.com.mgr.gestao.festa.dataprovider.model.Convidado;
@@ -15,7 +16,12 @@ public class ConvidadosController {
 	
 	@Autowired
 	private ConvidadosService service;
-	
+
+    @RequestMapping("/")
+    public String home() {
+        return "redirect:/login";
+    }
+    
 	@GetMapping("/convidados")
 	public ModelAndView listar() {
 
