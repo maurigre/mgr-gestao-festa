eb147d55315de878fc50d82ee7ecef0403c52027
maurigre@gmail.com <7UUjepGDa4yquMN__!@#$4321>
2021-03-14 22:01:43 -0300

    feat: incluir novos convidados

--- src/main/java/br/com/mgr/gestao/festa/controllers/ConvidadosController.java
+++ src/main/java/br/com/mgr/gestao/festa/controllers/ConvidadosController.java
@@ -3,8 +3,10 @@ package br.com.mgr.gestao.festa.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import br.com.mgr.gestao.festa.dataprovider.model.Convidado;
 import br.com.mgr.gestao.festa.dataprovider.services.convidados.ConvidadosService;
 
 
@@ -18,11 +20,15 @@ public class ConvidadosController {
 	public ModelAndView listar() {
 
 		ModelAndView view =	new ModelAndView("ListaConvidados");
+		view.addObject(new Convidado());
 		view.addObject("convidados", service.findAll());
 		return view;
 	}
 
-	
-	
+	@PostMapping("/convidado")
+	public String salvar(Convidado convidado) {
+		service.save(convidado);		
+		return "redirect:/convidados";		
+	}
 
 }
